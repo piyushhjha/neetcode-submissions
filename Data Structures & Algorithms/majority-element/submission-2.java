89791802a9cb6d1e129b921cfe4fc65bd78b39b6
Piyush Ranjan Jha <164197777+piyushhjha@users.noreply.github.com>
2026-07-05 22:39:03 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        // int n =nums.length;
+        // int k=0;
+        // for(int i=0;i<n; i++){
+        //     for(int j=i; j<n; j++){
+        //         if(nums[i]==nums[j]) k++;
+        //     }
+        //     if(k>n/2) return nums[i];
+        // }
+        // return k;
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+    }
+}
